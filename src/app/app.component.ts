17d5a1b269d86d5a018fa92af56cd71f50fa79ef
Greg Jose dos Santos <g.santos@dom_qp.com.br>
2018-03-27 13:29:20 -0300

    firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as fb from 'firebase'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit(): void {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyCfxGItRU-2qzWnYBzPQcDl6S64_AtjZrM",
+      authDomain: "app3-intragran.firebaseapp.com",
+      databaseURL: "https://app3-intragran.firebaseio.com",
+      projectId: "app3-intragran",
+      storageBucket: "app3-intragran.appspot.com",
+      messagingSenderId: "166067331066"
+    };
+    fb.initializeApp(config);
+  }
 }
--- src/app/shered/auth.service.ts
+++ src/app/shered/auth.service.ts
@@ -1,7 +1,19 @@
 import { Usuario } from "./usuario.model";
+import * as fb from 'firebase'
 
 export class Auth {
-    public cadastrarusuario (usuario: Usuario): void{
-        
+    public cadastrarusuario(usuario: Usuario): void {
+
+        fb.auth().createUserWithEmailAndPassword(usuario.email, usuario.senha)
+            .then((response: any) => { 
+                
+                // Removendo atributo senha do objeto   
+                delete usuario.senha;
+
+                // Registrando informações no Banco
+                fb.database().ref(`usuario_detalhe/${btoa(usuario.email)}`)
+                .set(usuario)
+            })
+            .catch((erro: Error) => console.log(erro));
     }
 }
